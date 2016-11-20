67a35fdaf4152ec51216435ec90d5e2ae3f372a7
Gilberto Marcon <gilbertoantoniomarcon@gmail.com>
2016-11-20 12:24:39 -0800

    Revised ploting scripts.

--- Plots/popPlot.m
+++ Plots/popPlot.m
@@ -3,19 +3,25 @@ close all;
 clc;
 
 % File Params
+TITLE = {'Local (L)', 'Global (G)', 'Difference (D)'};
 FILENAME = {'../LocalRwd/SYS_RWD', '../GlobalRwd/SYS_RWD', '../DiffRwd/SYS_RWD'};
 
 AV_WINDOW = 10;
 NUM_ROVERS = 2;
 
 figure;
-hold on;
-file = csvread(FILENAME{2});
-plot(max(file'));
-plot(mean(file'));
-plot(min(file'));
-xlabel('Generation');
-ylabel('System reward');
-grid on;
-axis tight;
-legend('max','mean','min')
+
+for i=1:3
+    subplot(3,1,i);
+    hold on;
+    file = csvread(FILENAME{i});
+    plot(max(file'));
+    plot(median(file'));
+    plot(min(file'));
+    xlabel('Generation');
+    ylabel('System reward');
+    grid on;
+    axis tight;
+    legend('max','median','min');
+    title(TITLE{i});
+end
